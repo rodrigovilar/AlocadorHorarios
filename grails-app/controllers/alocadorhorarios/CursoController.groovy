e2cc3bb9e99cd4752a08a4cde114d4724b4e4845
KalineEstevam <kaline.estevam@dce.ufpb.br>
2012-04-26 01:06:20 -0300

    alteracao nas classes Curso e CursoController

--- grails-app/controllers/alocadorhorarios/CursoController.groovy
+++ grails-app/controllers/alocadorhorarios/CursoController.groovy
@@ -3,4 +3,15 @@ package alocadorhorarios
 class CursoController {
 
     def scaffold = true
+	
+	def addPeriodos() {
+		
+		int i
+		Curso curso
+		
+		for (i = 1; i <= curso.getQuantidadeDePeriodos() ; i++) {
+			Periodo p = new Periodo(i)
+			curso.periodos.add(p)
+		} 
+	}
 }
--- grails-app/domain/alocadorhorarios/Curso.groovy
+++ grails-app/domain/alocadorhorarios/Curso.groovy
@@ -7,10 +7,13 @@ class Curso {
 	Professor viceCoordenador
 	static hasMany = [periodos:Periodo]
 	
+	Integer quantidadeDePeriodos
+	
     static constraints = {
 		nome()
 		coordenador(nullable:true)
 		viceCoordenador(nullable:true)
+		quantidadeDePeriodos(nullable:true)
     }
 
 	
