57dae872ecbbc008e6b198a9085d2c28794b421e
sergiocampos <sergio.campos@dce.ufpb.br>
2012-05-12 20:17:59 -0300

    testes funcionais Departamento-Curso-Disciplina-Professor

---
+++ test/functional/alocadorhorarios/TelefoneFunctionalTests.groovy
@@ -0,0 +1,42 @@
+package alocadorhorarios
+
+import com.grailsrocks.functionaltest.*
+
+class TelefoneFunctionalTests extends BrowserTestCase {
+    void testSomeWebsiteFeature() {
+		
+		get('/telefone/list')
+		assertContentContains "List"
+		assertStatus 200
+		
+		get('/telefone/create')
+		assertContentContains "Create"
+		assertStatus 200
+		
+		form {
+			numero "8332210011"
+			click "create"
+		}
+		assertContentContains "8332210011"
+		assertContentContains "created"
+		assertStatus 200
+		
+		click "Edit"
+		//assertContentContains "Edit"
+		//assertStatus 200
+		form {
+			numero "8132210011"
+			click "_action_update"
+		}
+		assertContentContains "8132210011"
+		assertContentContains "updated"
+		assertStatus 200
+		
+		click "Telefone List"
+		assertContentContains "8132210011"
+		assertContentContains "List"
+		assertStatus 200
+		
+		
+    }
+}
