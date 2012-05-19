4d7f5f3a475c53e0188a76aefe85ab8f73b772e5
sergiocampos <sergio.campos@dce.ufpb.br>
2012-05-18 22:44:48 -0300

    Teste Funcional Usuario [Login]

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,9 @@
+import alocadorhorarios.Usuario
+
 class BootStrap {
 
     def init = { servletContext ->
+		def Usuario = new Usuario (username: "Manuel", password: "1234")
     }
     def destroy = {
     }
---
+++ test/functional/alocadorhorarios/UsuarioFunctionalTests.groovy
@@ -0,0 +1,55 @@
+package alocadorhorarios
+
+import com.grailsrocks.functionaltest.*
+
+class UsuarioFunctionalTests extends BrowserTestCase {
+    void testLogin() {
+		get('/login/auth')
+		assertStatus 200
+		
+		form {
+			j_username "joao"
+			j_password "1234"
+			click "submit"
+		}
+		assertContentContains "submit"
+		assertStatus 200
+    }
+	void testUsuarioList(){
+		get('/usuario/list')
+		assertContentContains "List"
+		assertStatus 200
+		
+		get('/usuario/create')
+		assertStatus 200
+		form {
+			username "Manuel"
+			password "1234"
+			click "create"
+		}
+		assertStatus 200
+		assertContentContains "Manuel"
+		assertContentContains "created"
+		assertStatus 200
+		
+		
+		get('/usuario/list')
+		assertContentContains "List"
+		assertStatus 200
+		
+		/*click "Gonçalves"
+		assertContentContains "Gonçalves"
+		assertStatus 200
+		form {
+			j_username "Tiao"
+			j_password "senha00"
+			click "_action_update"
+		}
+		assertStatus 200
+		assertContentContains "Tiao"
+		assertContentContains "senha00"
+		assertContentContains "updated"
+		assertStatus 200
+		*/
+	}
+}
